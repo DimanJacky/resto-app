5aa8910d90014216338c2313021c8588dffa0570
DimanJacky <maslov-mda@yandex.ru>
2020-08-26 22:37:37 +0300

    go to lessons

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import {Provider} from 'react-redux';
-import {BrowserRouter as Router} from "react-router-dom";
-import ErrorBoundry from "./components/error-boundry";
-import RestoService from "./services/resto-service";
-import RestoServiceContext from "./components/resto-service-context";
-import store from "./store";
+import {BrowserRouter as Router} from 'react-router-dom';
+import ErrorBoundry from './components/error-boundry';
+import RestoService from './services/resto-service';
+import RestoServiceContext from './components/resto-service-context';
+import store from './store';
 
 import './index.scss';
+
 
 const restoService = new RestoService();
 
@@ -22,5 +23,6 @@ ReactDOM.render(
             </RestoServiceContext.Provider>
         </ErrorBoundry>
     </Provider>
-    , document.getElementById('root'));
+    , document.getElementById('root')
+);
 
